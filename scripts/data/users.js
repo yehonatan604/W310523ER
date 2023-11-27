1e7c749354b243365b5006865e8101ee52db24fa
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2023-11-27 20:12:48 +0200

    commit 8

---
+++ scripts/data/users.js
@@ -0,0 +1,23 @@
+let users = [
+    {
+        fname: 'Moshe',
+        lname: 'David',
+        email: 'moshe@email.com',
+        password: 'Qaz123!qaz',
+        isLogedIn: false
+    },
+    {
+        fname: 'Joe',
+        lname: 'Moe',
+        email: 'joe@email.com',
+        password: 'Qaz123!qaz',
+        isLogedIn: false
+    },
+    {
+        fname: 'Miriam',
+        lname: 'Gany-Chenya',
+        email: 'miriam@email.com',
+        password: 'Qaz123!qaz',
+        isLogedIn: false
+    }
+];
---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+const usersTable = document.querySelector(".users-table");
+const submitBtn = document.querySelector("#submitBtn");
+
+
+console.log(usersTable);
